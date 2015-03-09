7643f35f30e21b7b2890194ad167f694a9791485
rickyall <783814127@qq.com>
2015-03-09 18:43:42 +0800

    生成分页

--- routes/index.js
+++ routes/index.js
@@ -30,8 +30,12 @@ module.exports = function(app) {
 
  //话题
  app.get('/topic',function(req,res){
+  //判断是否是第一页
+  var page=req.query.p?parseInt(req.query.p):1;
+  
+  //查询并返回第page页的10篇文章
 
- 	Post.getAll(null, function(err,posts){
+ 	Post.getTen(null, page, function(err,posts,total){
 
  		if(err){
  			posts=[];
@@ -42,8 +46,11 @@ module.exports = function(app) {
  		title:'话题',
  		user: req.session.user,
  		posts: posts,
-        success: req.flash('success').toString(),
-        error: req.flash('error').toString()
+    page:page,
+    isFirstPage:(page-1)==0,
+    isLastPage:((page-1)*10+posts.length)==total,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
  	});
  });
 });
@@ -67,20 +74,29 @@ module.exports = function(app) {
 
 app.get('/user/:name',function(req,res){
 
+   var page = req.query.p ? parseInt(req.query.p) : 1;
+
   //检查用户名是否存在
   User.get(req.params.name,function(err,user){
     if(!user){
       req.flash('error','用户不存在！');
       return res.redirect('/');
     }
-    Post.getAll(user.name,function(err,posts){
+
+    //查询并返回该用户的第PAGE页的10篇文章
+
+
+    Post.getTen(user.name,page,function(err,posts,total){
       if(err){
         req.flash('error',err);
-        return res.redirect('/;')
+        return res.redirect('/');
       }
       res.render('user/user',{
         title:user.name,
         posts:posts,
+        page:page,
+        isFirstPage:(page-1)==0,
+        isLastPage:((page-1)*10+posts.length)==total,
         user:req.session.user,
         success:req.flash('success').toString(),
         error:req.flash('error').toString()
